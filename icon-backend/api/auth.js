f459c1c37939d8eb7b222760e6e3155cc15d9a7f
Lestat <2233915@slu.edu.ph>
2025-06-16 16:23:29 +0800

    Refactor authentication router to improve structure and update health check response

--- icon-backend/api/auth.js
+++ icon-backend/api/auth.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const {authenticateUser} = require('../dal/userDAL.js')
 
-const apiRouter = express.Router()
-apiRouter.use(express.json())
+const authRouter = express.Router()
+authRouter.use(express.json())
 
-apiRouter.get('/health', (req, res) => {
-    res.json({"message":"events api healthy"})
+authRouter.get('/health', (req, res) => {
+    res.json({"message":"auth api healthy"})
 })
 
+authRouter.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    try {
+        const user = await authenticateUser(username, password)
+        res.status(200).json({ message: 'Login successful', user })
+    } catch (error) {
+        console.error('Login error:', error)
+        res.status(401).json({ message: 'Invalid credentials' })
+    }
+})
 
-module.exports = apiRouter
+module.exports = authRouter
