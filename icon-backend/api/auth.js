0bc98fca59cfd88646915261be3f3f58d7f19283
Lestat <2233915@slu.edu.ph>
2025-06-21 23:41:51 +0800

    feat(auth): enhance login endpoint to return JWT token and user details

--- icon-backend/api/auth.js
+++ icon-backend/api/auth.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const {authenticateUser} = require('../dal/userDAL.js')
+const { generateToken } = require('../services/jwt.js')
+
 
 const authRouter = express.Router()
 authRouter.use(express.json())
@@ -12,7 +14,19 @@ authRouter.post('/login', async (req, res) => {
     const { username, password } = req.body
     try {
         const user = await authenticateUser(username, password)
-        res.status(200).json({ message: 'Login successful', user })
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' })
+        }
+
+        const token = generateToken(user)
+
+        res.status(200).json({ 
+            message: 'Login successful',
+            token: token,
+            name: user.name,
+            username: user.username,
+        })
+        
     } catch (error) {
         console.error('Login error:', error)
         res.status(401).json({ message: 'Invalid credentials' })
