ae7113531f57622a24e8a313601d338d55cffdb8
Lestat <2233915@slu.edu.ph>
2025-06-21 22:41:00 +0800

    chore(eventsDAL):  add helper functions for role checking and event data validation

--- icon-backend/dal/eventsDAL.js
+++ icon-backend/dal/eventsDAL.js
@@ -32,6 +32,37 @@ async function updateEventById(eventId, eventData, userId ,role) {
 
 }
 
+function checkRole(role, requiredRole) {
+    if (role !== requiredRole) {
+        throw new Error(`Unauthorized: ${role} does not have permission to perform this action`);
+    }
+}
+
+function validateEventData(eventData) {
+    if (!eventData.name){
+        throw new Error('Event name is required');
+    }
+
+    if (!eventData.description) {
+        throw new Error('Event description is required');
+    }
+
+    if (!eventData.date) {
+        throw new Error('Event date is required');
+    }
+
+    if(!eventData.createdBy){
+        throw new Error ('Event must have a creator')
+    }
+
+    if(!eventData.organizingCommittee) {
+        throw new Error('Event must have an organizing committee');
+    }
+}   
+
+
+
+
 
 
 module.exports = {
