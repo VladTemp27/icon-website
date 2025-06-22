16bee88c9fd41ea952b0359b09c701f6f1a23f82
Lestat <2233915@slu.edu.ph>
2025-06-22 15:42:09 +0800

    feat(events): implement event handling functions and set up event routes

---
+++ icon-backend/api/controller/eventsController.js
@@ -0,0 +1,47 @@
+const {
+    createEvent,
+    getEventById,
+    getAllEvents,
+    getActiveEvents,
+    getNonActiveEvents,
+    getEventsOnFilter,
+    deleteEventById,
+    updateEventById
+} = require('../../dal/eventsDAL.js')
+
+async function handleCreate(req, res) {
+
+}
+
+async function handleGetById(req, res) {
+
+}
+
+async function handleGetAll(req, res) {
+
+}
+
+async function handleGetActive(req, res) {
+
+}
+
+async function handleGetNonActive(req, res) {
+
+}
+
+async function handleDeletebyEventId(req, res) {
+
+}
+
+async function handleUpdateByEventId(req, res) {
+}
+
+module.exports = {
+    handleCreate,
+    handleGetById,
+    handleGetAll,
+    handleGetActive,
+    handleGetNonActive,
+    handleDeletebyEventId,
+    handleUpdateByEventId
+}
--- icon-backend/api/events.js
+++ icon-backend/api/events.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const eventsController = require('./controller/eventsController')
+const { handleCreate } = require('./controller/eventsController.js')
+const { authorize } = require('./middleware/authorize.js')
+const { events } = require('../models/User.model.js')
 
 const eventsRouter = express.Router()
 eventsRouter.use(express.json())
+
+eventsRouter.post('/create', authorize('officer', 'executive'), eventsController.handleCreate)
+eventsRouter.get('/:eventId', eventsController.handleGetById)
+eventsRouter.get('/all', authorize('officer', 'executive'), eventsController.handleGetAll)
+eventsRouter.get('/active', eventsController.handleGetActive)
+eventsRouter.get('/non-active', authorize('officer', 'executive'), eventsController.handleGetNonActive)
+eventsRouter.delete('/:eventId', authorize('officer', 'executive'), eventsController.handleDeletebyEventId)
+eventsRouter.put('/:eventId', authorize('officer', 'executive'), eventsController.handleUpdateByEventId)
+
 
 eventsRouter.get('/health', (req, res) => {
     res.json({"message":"events api healthy"})
