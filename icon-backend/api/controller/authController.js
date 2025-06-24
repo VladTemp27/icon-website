8dc9b2bf44ea7f97e5d42c669bec32fcf634b74a
Lestat <2233915@slu.edu.ph>
2025-06-24 17:15:35 +0800

    refactor(auth): enhance login handling with Redis for tracking login attempts

--- icon-backend/api/controller/authController.js
+++ icon-backend/api/controller/authController.js
@@ -1,17 +1,22 @@
 const {authenticateUser} = require('../../dal/userDAL.js')
 const { generateToken, verifyToken } = require('../../util/jwt.js')
+const redisClient = require('../../util/redisClient.js')
+
+const LOGIN_TIMEOUT = 60 * 30 // 30 minutes
+const MAX_LOGIN_ATTEMPTS = 5
 
 
 async function handleLogin(req, res) {
     const { username, password } = req.body
+    const attempt = parseInt(await redisClient.get(`login_attempts:${username}`)) || 0
     try {
-        const user = await authenticateUser(username, password)
-        if (!user) {
-            return res.status(401).json({ message: 'Invalid credentials' })
+        if(attempt >= MAX_LOGIN_ATTEMPTS) {
+            return res.status(429).json({ message: 'Too many login attempts. Please try again later.' })
         }
+        const user = await authenticateUser(username, password)
 
         const token = generateToken(user)
-
+        redisClient.del(`login_attempts:${username}`)
         res.status(200).json({ 
             message: 'Login successful',
             token: token,
@@ -20,7 +25,8 @@ async function handleLogin(req, res) {
         })
 
     } catch (error) {
-        console.error('Login error:', error)
+        redisClient.set(`login_attempts:${username}`, attempt+1);
+        redisClient.expire(`login_attempts:${username}`, LOGIN_TIMEOUT)
         res.status(401).json({ message: 'Invalid credentials' })
     }
 }
