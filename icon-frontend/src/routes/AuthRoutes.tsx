c71e05c406d720097c3dc87875f5d1261a4d115d
Lestat <2233915@slu.edu.ph>
2025-06-25 13:54:42 +0800

    feat: update AuthRouter to use AuthLayout and Login components

--- icon-frontend/src/routes/AuthRoutes.tsx
+++ icon-frontend/src/routes/AuthRoutes.tsx
@@ -1,13 +1,13 @@
 import {Route} from 'react-router';
+import Login from '../components/login/Login';
+import AuthLayout from '../components/layout/AuthLayout';
 
 const AuthRouter = (
-    <Route path="/auth" element={null}>
-        <Route index element={<h1>404</h1>} />
-        <Route path="*" element={<h1>404</h1>} />
-        <Route path="login" element={<h1>login</h1>} />
+    <Route path="/auth" element={<AuthLayout />}>
+        <Route index element={<Login/>} />
+        <Route path="login" element={<Login/>} />
         <Route path="register" element={<h1>register</h1>} />
         <Route path="forgot-password" element={<h1>forgot-password</h1>} />
-        <Route path="reset-password" element={null} />
     </Route>
 );
 
