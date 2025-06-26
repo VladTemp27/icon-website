4e1d4580945ea1ebe3a6b2ae64a149ff3c8964c1
Lestat <2233915@slu.edu.ph>
2025-06-26 09:42:24 +0800

    fix(authUtils): fix bug with checkAuth returning 401 when 200

--- icon-frontend/src/util/authUtils.tsx
+++ icon-frontend/src/util/authUtils.tsx
@@ -12,10 +12,12 @@ export const RedirectIfAuthenticated = ({ children }: { children: JSX.Element })
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (token) {
+            console.log('Token found, checking authentication...');
             checkAuth(token).then((result) => {
                 setIsAuthenticated(result);
             });
         } else {
+            console.log('No token found, user is not authenticated.');
             setIsAuthenticated(false);
         }
     }, []);
@@ -56,14 +58,20 @@ export const RequireAuth = ({ children }: { children: JSX.Element }) => {
     return children;
 };
 
+// This function checks if the user is authenticated by verifying the token
+// It returns true if the token is valid, otherwise false
 async function checkAuth(token: string | null): Promise<boolean> {
+    if (!token){
+        return false;
+    }
     try{
-        console.log('Checking authentication with token:', token);
-        const response = await axios.post('http://localhost:1525/api/auth/verify', {
+        const response = await axios.post('http://localhost:1525/api/auth/verify',{} ,{
             headers: {
                 "Authorization": `Bearer ${token}`
             }
         });
+
+        console.log(response.data)
 
         if(response.status === 200) {
             return true;
