d5fdf064ec00dccbeba1fe7588ea72c93fa032e4
Lestat <2233915@slu.edu.ph>
2025-06-27 14:31:24 +0800

    feat(routes): update HomeRoutes to include additional paths and utilize IndexRedirect and CheckRole for improved navigation

--- icon-frontend/src/routes/HomeRoutes.tsx
+++ icon-frontend/src/routes/HomeRoutes.tsx
@@ -3,13 +3,20 @@ import { Route, Routes } from 'react-router';
 import HomeLayout from '../components/layout/HomeLayout';
 import {RequireAuth} from '../util/authUtils'
 
+import { IndexRedirect, CheckRole } from '../util/home-router-helper';
+import Loading from '../pages/Loading';
+
 const HomeRoutes = (
         <Route path='/home' element={<RequireAuth><HomeLayout /></RequireAuth>}>
-            <Route index element={<h1>Home</h1>} />
-            <Route path='dashboard' element={<h1>Dashboard</h1>} />
+            <Route index element={<IndexRedirect/>} />
+            <Route path='dashboard' element={<CheckRole requiredRole={['executive, officer']}><Loading/></CheckRole>} />
             <Route path='settings' element={<h1>Settings</h1>} />
             <Route path='profile' element={<h1>Profile</h1>} />
             <Route path='help' element={<h1>Help</h1>} />
+            <Route path='events' element={<h1>Events</h1>} />
+            <Route path='leaderboards' element={<h1>Leaderboards</h1>} />
+            <Route path='payments' element={<h1>Payments</h1>} />
+            <Route path='announcements' element={<h1>Announcements</h1>} />
         </Route>
     )
 
