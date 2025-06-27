78d079b2c152a0b417bc37b693c6d6931dde27ef
Lestat <2233915@slu.edu.ph>
2025-06-27 14:31:11 +0800

    feat(sidebar): implement getMenuItems function for dynamic sidebar navigation based on user role

---
+++ icon-frontend/src/components/sidebar/MenuItem.tsx
@@ -0,0 +1,86 @@
+import { useNavigate } from 'react-router';
+
+import {
+  DashboardOutlined,
+  MoneyCollectOutlined,
+  FundViewOutlined,
+  CalendarOutlined,
+  BellOutlined
+} from '@ant-design/icons';
+
+function getMenuItems() {
+    const navigate = useNavigate();
+    const role = localStorage.getItem('role');
+
+    const executiveMenuItems = [
+    {
+        key: 'dashboard',
+        label: 'Dashboard',
+        icon: <DashboardOutlined style={{fontSize: '20px'}} />,
+        onClick: () => navigate('/home/dashboard')
+    },
+    {
+        key: 'events',
+        label: 'Events',
+        icon: <CalendarOutlined style={{fontSize: '20px'}}/>,
+        onClick: () => navigate('/home/events')
+    },
+    {
+        key: 'leaderboards',
+        label: 'Leaderboards',
+        icon: <FundViewOutlined style={{fontSize: '20px'}}/>,
+        onClick: () => navigate('/home/leaderboards')
+    },
+    {
+        key: 'payments', 
+        label: 'Payments',
+        icon: <MoneyCollectOutlined style={{fontSize: '20px'}}/>,
+        onClick: () => navigate('/home/payments')
+    },
+    {
+        key: 'announcements',
+        label: 'Announcements',
+        icon: <BellOutlined style={{fontSize: '20px'}}/>,
+        onClick: () => navigate('/home/announcements')
+    }
+]
+
+const memberMenuItems = [
+    {
+        key: 'events',
+        label: 'Events',
+        icon: <CalendarOutlined  style={{fontSize: '20px'}}/>,
+        onClick: () => navigate('/home/events')
+    },
+    {
+        key: 'leaderboards',
+        label: 'Leaderboards',
+        icon: <FundViewOutlined  style={{fontSize: '20px'}}/>,
+        onClick: () => navigate('/home/leaderboards')
+    },
+    {
+        key: 'payments', 
+        label: 'Payments',
+        icon: <MoneyCollectOutlined  style={{fontSize: '20px'}}/>,
+        onClick: () => navigate('/home/payments')
+    },
+    {
+        key: 'announcements',
+        label: 'Announcements',
+        icon: <BellOutlined  style={{fontSize: '20px'}}/>,
+        onClick: () => navigate('/home/announcements')
+    }
+]
+
+
+    switch( role ) {
+        case 'executive':
+            return executiveMenuItems
+        case 'officer':
+            break;
+        case 'member':
+            return memberMenuItems
+    }
+}
+
+export default getMenuItems;
